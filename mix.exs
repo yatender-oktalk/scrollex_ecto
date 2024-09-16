385afec6cb63302a090af777f81c7e66a2cab66e
Yatender Singh <yatender.nitk@gmail.com>
2024-09-16 22:41:25 +0200

    chore: :fire: Fix license file typo
    Fixed typo in the file and readme link

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ScrollexEcto.MixProject do
   use Mix.Project
 
-  @version "0.1.0+minor"
+  @version "0.1.0"
   @source_url "https://github.com/yatender-oktalk/scrollex_ecto"
 
   def project do
